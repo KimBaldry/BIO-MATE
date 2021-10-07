3846a7058e5e81561de6df5308a0a91867ba7782
Kim <baldry.kimberlee@gmail.com>
2021-10-08 10:20:35 +1100

    BIO-SHARE development

--- src/BIO-SHARE/app.R
+++ src/BIO-SHARE/app.R
@@ -10,6 +10,12 @@ library(shiny)
 library(shinyWidgets)
 library(dplyr)
 library(rhandsontable)
+
+load(url("https://github.com/KimBaldry/BIO-MATE/raw/main/data_descriptor_paper/metadata_info.RData"))
+
+p_meta$`User input` = ""
+p_meta = p_meta[,c(1:2,5,4:3)]
+
 # 1. user interface object
 ui <- navbarPage(
  "BIO-SHARE",
@@ -17,7 +23,8 @@ ui <- navbarPage(
            fluidPage(
              p("Upload published data files. BIO-SHARE can only handle processing one data stream at a time."),
              fileInput("upload_PROF", NULL, accept = c(".csv", ".tsv",".tab",".nc",".all",".sb",".txt"),multiple = T), 
-             selectInput("stream", label = "Data stream to run", choices = c("PROF", "PIG")),
+             #selectInput("run_expocode", label = "EXPOCODE", choices(c("new", ))) # reference to existing expocodes in package metadata
+             selectInput("stream", label = "Data stream to run", choices = c("PROF", "PIG"))
            )),
   tabPanel("Split files",
            fluidPage(
@@ -40,16 +47,32 @@ ui <- navbarPage(
              p("Only needed if Split by station = T. The variable containing station IDs"),
            selectInput("fillcell", "Fill cells", choices = c("T","F"), selected = "F"),
            p("logical. If TRUE then in case the rows have unequal length, blank fields are implicitly added. See 'fill' and 'Details' in 'read.table' for more information.")
-           )
-           
-           ), 
+           )), 
+ tabPanel("Upload BibTEX files", 
+          fluidPage(
+            p("Profiling sensor citation"),
+            
+            p("Pigment data citation"), 
+            
+            p("Source citation"),
+            
+            p("Pigment method")
+          )), 
   tabPanel("Enter metadata",
-           fluidPage(
-             rHandsontableOutput("hot")
+           fluidPage(#p("EXPOCODE metadata") # check dates and ship to see if exists in system
+             p("File format information"),
+             rHandsontableOutput("hot1"),
+             p("Data aquisition information"),
+             rHandsontableOutput("hot2"),
+             p("Location Data information"),
+             rHandsontableOutput("hot3"),
+             conditionalPanel(condition = "input.stream == 'PROF'",p("Profiling sensor data information"),
+             rHandsontableOutput("hot4")),
+             conditionalPanel(condition = "input.stream == 'PIG'", p("Pigment and POC data information"),
+             rHandsontableOutput("hot5"))
            )
            ),
-  tabPanel("Upload BibTEX files"
-           ), 
+
   tabPanel("Run BIOMATE"
            ),
   tabPanel("Upload to GitHub"
@@ -66,8 +89,18 @@ ui <- navbarPage(
 )
 
 # 2.server function
+# example here https://cran.r-project.org/web/packages/rhandsontable/vignettes/intro_rhandsontable.html
+col.hi.render = "
+    function(instance, td) {
+Handsontable.renderers.TextRenderer.apply(this, arguments);
+td.style.background = 'lightblue';
+}"
+
+
 server <- function(input, output){
-  load(url("https://github.com/KimBaldry/BIO-MATE/raw/main/data_descriptor_paper/metadata_info.RData"))
+  # check existence of EXPOCODE and stream data in the system
+
+  
   file_paths = reactive({ # this should be where the files are stored not their names 
     req(input$upload)
     
@@ -77,12 +110,38 @@ server <- function(input, output){
   })
   
 #  split_delim_file(dirname(file_paths), input$upload$name, input$delim, input$line_start, input$expo_split, input$synonym_var_name, input$station_split, input$station_var_name, input$fillcell)
+
   
+  #### Enter Metadata ########  
   values <- reactiveValues()
   observe({
-    if (!is.null(input$hot)) {
-      p_meta = hot_to_r(input$hot)
-    } else {
+    if(any(!is.null(input$hot1), !is.null(input$hot2), !is.null(input$hot3), !is.null(input$hot4), !is.null(input$hot5))){
+      if(!is.null(input$hot1)){
+        temp = hot_to_r(input$hot1)
+        idx = unlist(lapply(temp[,1], function(x){which(p_meta[,1] == x)}))
+        p_meta[idx,] = hot_to_r(input$hot1)
+      }
+      if(!is.null(input$hot2)){
+        temp = hot_to_r(input$hot2)
+        idx = unlist(lapply(temp[,1], function(x){which(p_meta[,1] == x)}))
+        p_meta[idx,] = hot_to_r(input$hot2)
+      }
+      if(!is.null(input$hot3)){
+        temp = hot_to_r(input$hot3)
+        idx = unlist(lapply(temp[,1], function(x){which(p_meta[,1] == x)}))
+        p_meta[idx,] = hot_to_r(input$hot3)
+      }
+      if(!is.null(input$hot4)){
+        temp = hot_to_r(input$hot4)
+        idx = unlist(lapply(temp[,1], function(x){which(p_meta[,1] == x)}))
+        p_meta[idx,] = hot_to_r(input$hot4)
+      }
+      if(!is.null(input$hot5)){
+        temp = hot_to_r(input$hot5)
+        idx = unlist(lapply(temp[,1], function(x){which(p_meta[,1] == x)}))
+        p_meta[idx,] = hot_to_r(input$hot5)
+      }
+      } else {
       if (is.null(values[["p_meta"]]))
         p_meta <- p_meta
       else
@@ -91,9 +150,31 @@ server <- function(input, output){
     values[["p_meta"]] <- p_meta
   })
   
+  # possible selection options for fixed when have time later. Including check on EXPOCODE
   
-  output$hot =  renderRHandsontable({p_meta$`User entry` = "" # make this column column # 3 and somehow change width of column
-    rhandsontable(p_meta) %>% hot_col(c(1:4), readOnly = TRUE)})
+  output$hot1 =  renderRHandsontable({
+    p_meta = values[["p_meta"]]
+    tab = p_meta[1:7,] %>% filter(`Data stream/s` == "all" | grepl(input$stream, `Data stream/s`, ignore.case = T))
+    rhandsontable(tab, width = 1200) %>% hot_col(c(1:2,4:5), readOnly = TRUE) %>% hot_table(highlightRow = TRUE) %>%  hot_context_menu(allowRowEdit = FALSE, allowColEdit = FALSE) %>% 
+      hot_cols(colWidths = c(200, 100, 200, 250, 400)) %>% hot_col("User input", renderer = col.hi.render)})
+  
+  output$hot2 =  renderRHandsontable({tab = p_meta[8:15,] %>% filter(`Data stream/s` == "all" | grepl(input$stream, `Data stream/s`, ignore.case = T))
+    rhandsontable(tab, width = 1200, row_highlight = c(1,2)) %>% hot_col(c(1:2,4:5), readOnly = TRUE) %>% hot_table(highlightRow = TRUE) %>%  hot_context_menu(allowRowEdit = FALSE, allowColEdit = FALSE) %>% hot_cols(colWidths = c(200, 100, 200, 250, 400)) %>% hot_col("User input", renderer = col.hi.render)})
+  
+  output$hot3 =  renderRHandsontable({tab = p_meta[16:41,] %>% filter(`Data stream/s` == "all" | grepl(input$stream, `Data stream/s`, ignore.case = T))
+    rhandsontable(tab, width = 1200) %>% hot_col(c(1:2,4:5), readOnly = TRUE) %>% hot_table(highlightRow = TRUE) %>%  hot_context_menu(allowRowEdit = FALSE, allowColEdit = FALSE) %>% 
+      hot_cols(colWidths = c(200, 100, 200, 250, 400)) %>% hot_col("User input", renderer = col.hi.render)})
+  
+  output$hot4 =  renderRHandsontable({tab = p_meta[42:61,] %>% filter(`Data stream/s` == "all" | grepl(input$stream, `Data stream/s`, ignore.case = T))
+    rhandsontable(tab, width = 1200) %>% hot_col(c(1:2,4:5), readOnly = TRUE) %>% hot_table(highlightRow = TRUE) %>%  hot_context_menu(allowRowEdit = FALSE, allowColEdit = FALSE) %>% 
+      hot_cols(colWidths = c(200, 100, 200, 250, 400)) %>% hot_col("User input", renderer = col.hi.render)})
+  
+  output$hot5 =  renderRHandsontable({tab = p_meta[62:105,] %>% filter(`Data stream/s` == "all" | grepl(input$stream, `Data stream/s`, ignore.case = T))
+    rhandsontable(tab, width = 1200) %>% hot_col(c(1:2,4:5), readOnly = TRUE) %>% hot_table(highlightRow = TRUE) %>%  hot_context_menu(allowRowEdit = FALSE, allowColEdit = FALSE) %>% 
+      hot_cols(colWidths = c(200, 100, 200, 250, 400)) %>% hot_col("User input", renderer = col.hi.render)})
+  
+  ####### Download Files ########
+  
   # output$download <- downloadHandler(
   #   filename = function(){
   #     paste0(input$text,".zip")
